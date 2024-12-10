f15384556ac699aeb5127deff7c59065dc375cf1
DaeunYoon <daa3230@gmail.com>
2024-12-10 10:24:19 +0100

    fix: env key

--- frontend/src/lib/authHelpers.ts
+++ frontend/src/lib/authHelpers.ts
@@ -1,48 +1,9 @@
 import jwt, { JwtPayload, Secret, VerifyErrors } from 'jsonwebtoken'
 
-export const validateJWT = async (
-  token: string
-): Promise<JwtPayload | null> => {
-  try {
-    const decodedToken =
-      ((await new Promise()) < JwtPayload) |
-      (null >
-        ((resolve, reject) => {
-          jwt.verify(
-            token,
-            getKey,
-            { algorithms: ['RS256'] },
-            (
-              err: VerifyErrors | null,
-              decoded: string | JwtPayload | undefined
-            ) => {
-              console.log('decoded the jwt')
-              if (err) {
-                reject(err)
-              } else {
-                // Ensure that the decoded token is of type JwtPayload
-                if (typeof decoded === 'object' && decoded !== null) {
-                  resolve(decoded)
-                } else {
-                  reject(new Error('Invalid token'))
-                }
-              }
-            }
-          )
-        }))
-    return decodedToken
-  } catch (error) {
-    console.error('Invalid token:', error)
-    return null
-  }
-}
-
 export const getKey = (
-  headers,
+  headers: any,
   callback: (err: Error | null, key?: Secret) => void
 ): void => {
-  console.log('calling getKey')
-
   // Define the options for the fetch request
   const options = {
     method: 'GET',
@@ -53,7 +14,7 @@ export const getKey = (
 
   // Perform the fetch request asynchronously
   fetch(
-    `https://app.dynamicauth.com/api/v0/environments/${process.env.NEXT_PUBLIC_DYNAMIC_ENVIRONMENT_ID}/keys`,
+    `https://app.dynamicauth.com/api/v0/environments/${process.env.NEXT_PUBLIC_DYNAMIC_ENV_ID}/keys`,
     options
   )
     .then((response) => {
@@ -61,11 +22,49 @@ export const getKey = (
     })
     .then((json) => {
       const publicKey = json.key.publicKey
+
       const pemPublicKey = Buffer.from(publicKey, 'base64').toString('ascii')
-      callback(null, pemPublicKey) // Pass the public key to the callback
+
+      callback(null, pemPublicKey)
     })
     .catch((err) => {
-      console.error(err)
-      callback(err) // Pass the error to the callback
+      console.error('Error fetching public key:', err)
+      callback(err)
     })
+}
+
+export const validateJWT = async (
+  token: string
+): Promise<JwtPayload | null> => {
+  try {
+    const decodedToken = await new Promise<JwtPayload | null>(
+      (resolve, reject) => {
+        jwt.verify(
+          token.trim(),
+          getKey,
+          { algorithms: ['RS256'] },
+          (
+            err: VerifyErrors | null,
+            decoded: string | JwtPayload | undefined
+          ) => {
+            if (err) {
+              console.log('JWT verification error:', err)
+              reject(err)
+            } else {
+              console.log('JWT successfully decoded')
+              if (typeof decoded === 'object' && decoded !== null) {
+                resolve(decoded)
+              } else {
+                reject(new Error('Invalid token'))
+              }
+            }
+          }
+        )
+      }
+    )
+    return decodedToken
+  } catch (error) {
+    console.error('Invalid token:', error)
+    return null
+  }
 }
