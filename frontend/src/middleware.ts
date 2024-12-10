8fc78cc63617f6c58b199b89f40160fb76e3f903
DaeunYoon <daa3230@gmail.com>
2024-12-10 23:02:48 +0100

    add middleware

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -1,1 +1,12 @@
-export { auth as middleware } from '@/auth'
+import { auth } from '@/auth'
+import { NextResponse } from 'next/server'
+
+export default auth((req) => {
+  if (req.nextUrl.pathname.startsWith('/dashboard') && !req.auth?.user) {
+    console.log('here')
+    return NextResponse.redirect(new URL('/', req.url))
+  }
+  if (req.nextUrl.pathname.startsWith('/login') && req.auth?.user) {
+    return NextResponse.redirect(new URL('/', req.url))
+  }
+})
