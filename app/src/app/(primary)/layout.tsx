28670c30a3bd849684599cd56379b05d56038615
gedithejedi <33606955+gedithejedi@users.noreply.github.com>
2024-12-22 23:05:23 +0100

    fix: footer text

--- app/src/app/(primary)/layout.tsx
+++ app/src/app/(primary)/layout.tsx
@@ -1,17 +1,26 @@
+'use client'
+
+import Link from 'next/link'
+import { useAccount } from 'wagmi'
+
 export default function MainLayout({
   children,
 }: Readonly<{
   children: React.ReactNode
 }>) {
+  const { isConnected } = useAccount()
+
   return (
     <div className="flex justify-center w-full min-h-screen bg-base-200 font-[family-name:var(--font-geist-sans)] p-4">
       <div className="flex items-center flex-col w-full min-h-full max-w-[1024px] py-4">
         <header className="flex w-full">
-          <span className="text-3xl font-bold">XioPay</span>
+          <Link href={isConnected ? `/dashboard` : `/`}>
+            <span className="text-3xl font-bold">XioPay</span>
+          </Link>
         </header>
         <div className="py-8 flex-1 w-full h-full">{children}</div>
         <footer className="text-center">
-          <p>Built with 💚 for Xion Believathon</p>
+          <p>Built with 💚 for Neo Hackathon</p>
         </footer>
       </div>
     </div>
