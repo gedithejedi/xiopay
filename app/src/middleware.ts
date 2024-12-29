957dc1aaf4a8aed9a09c3e246322b256f310ba7a
DaeunYoon <daa3230@gmail.com>
2024-12-29 22:33:58 +0100

    feat: protect apis

--- app/src/middleware.ts
+++ app/src/middleware.ts
@@ -8,4 +8,10 @@ export default auth((req) => {
   if (req.nextUrl.pathname.startsWith('/login') && req.auth?.user) {
     return NextResponse.redirect(new URL('/', req.url))
   }
+  if (req.nextUrl.pathname.startsWith('/api/auth')) {
+    return NextResponse.next()
+  }
+  if (req.nextUrl.pathname.startsWith('/api') && !req.auth?.user) {
+    return NextResponse.json({ error: 'Unauthorized' }, { status: 401 })
+  }
 })
