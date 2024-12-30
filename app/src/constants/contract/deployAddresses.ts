946cf4d8b3009fc4e95861096437f433a4915021
DaeunYoon <daa3230@gmail.com>
2024-12-30 18:43:06 +0100

    feat: update address

--- app/src/constants/contract/deployAddresses.ts
+++ app/src/constants/contract/deployAddresses.ts
@@ -5,7 +5,7 @@ export const getCampaignDeploymentAddress = (chain: Chain) => {
     case Chain.NEOX_TESTNET:
       return '0x7cd746824a59E733744560eba905A08e6db5dC97'
     case Chain.NEOX:
-      return '0x745f25014b12182de708f011338908759a267e6b'
+      return '0x93939291f9cFeFBbd88D01E8D725f751DE93d331'
     case Chain.SEPOLIA:
       return '0x213127ea5c3486d4a91809be9ff69d10cb5c6173'
     default:
@@ -18,7 +18,7 @@ export const getTokenAddress = (chain: Chain) => {
     case Chain.NEOX_TESTNET:
       return '0x5e2D16a78b614b71eb74feCE22A766eC631C13e3'
     case Chain.NEOX:
-      return '0x080819b2acc896375d2623fb4d658ee43bb6bd59'
+      return '0x91262f08f2681a3150e6be2f36a2d0cb4b738f34'
     case Chain.SEPOLIA:
       return '0x213127ea5c3486d4a91809be9ff69d10cb5c6173'
     default:
